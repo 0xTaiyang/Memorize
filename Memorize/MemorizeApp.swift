e6cb55889143d204f01a5de0cbde8dd8653aee80
0xTaiyang <taiyanglyu@icloud.com>
2024-06-15 13:18:05 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Taiyang Lyu on 15/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
